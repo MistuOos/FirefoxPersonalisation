a9740c2eb90048e31c81a40ab2eb984c2d6324bd
MistuOos <theomagre04@gmail.com>
2024-07-27 18:49:42 +0200

    test loading screen 3

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', () => {
-    const video = document.querySelector('video');
-    const body = document.querySelector('body');
-
-    video.addEventListener('loadeddata', () => {
-        body.style.backgroundImage = 'none'; // Masquer l'image de fond une fois la vidéo chargée
-    });
-});
